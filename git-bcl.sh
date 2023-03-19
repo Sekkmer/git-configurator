aa3adf0a1f06bbf49432239a7e05d1f4b0f9fac8
Sekkmer <sekkmer@gmail.com>
2023-03-19 22:45:01 +0100

    fix typo in git branch cleaner

--- git-bcl.sh
+++ git-bcl.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 git fetch --prune
 
-for branch on $(git for-each-ref --format='%(refname) %(upstream:track' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do
+for branch in $(git for-each-ref --format='%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do
 	git branch -D "$branch"
 done
