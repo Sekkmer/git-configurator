6274e565ee2a71cba390b24cbdc9dedf5e9c9e41
Sekkmer <sekkmer@gmail.com>
2023-04-08 18:28:45 +0200

    formatted git log aliases

--- git-select-commit.sh
+++ git-select-commit.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-git log --graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+git log --graph --color=always --format="%C(auto)%h%d %s %C(green)(%an) %C(reset)%C(black bold)%cr" "$@" |
 	sk --ansi --no-sort --reverse --print-query --expect=ctrl-d \
 		--preview 'echo {} | grep -m 1 -oE "[a-z0-9]+" | head -1 | xargs git show --color=always' |
 	tail -1 |
