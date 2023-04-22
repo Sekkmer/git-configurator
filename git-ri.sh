808c44293fa48559489adb27de0a43e3f6d911e5
Sekkmer <sekkmer@gmail.com>
2023-04-22 13:13:07 +0200

    add auto rebase target based on fixup commits

--- git-ri.sh
+++ git-ri.sh
@@ -16,6 +16,32 @@ else
 	drop="drop"
 fi
 
+function auto_rebase() {
+	earliest_target_hash=""
+	earliest_target_timestamp=""
+
+	for line in git log --pretty=oneline --abbrev-commit --grep="^fixup\!\|^squash\!" --all; do
+		commit_hash=$(echo "$line" | awk '{print $1}')
+		commit_msg=$(echo "$line" | awk '{$1=""; print $0}')
+
+		target_hash=$(echo "${commit_msg#*!}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+		target_hash_full=$(git rev-parse --verify "$target_hash")
+
+		if [ -z "$earliest_target_hash" ]; then
+			earliest_target_hash="$target_hash_full"
+			earliest_target_timestamp=$(git show -s --format=%ct "$target_hash_full")
+		else
+			current_target_timestamp=$(git show -s --format=%ct "$target_hash_full")
+			if [ "$current_target_timestamp" -lt "$earliest_target_timestamp" ]; then
+				earliest_target_hash="$target_hash_full"
+				earliest_target_timestamp="$current_target_timestamp"
+			fi
+		fi
+	done
+
+	echo "$earliest_target_hash"
+}
+
 case "${1-}" in
 e | edit | -e | --edit)
 	export GIT_SEQUENCE_EDITOR="sed -i -e '1s/^/$pick /$edit /'"
@@ -25,6 +51,10 @@ r | reword | -r | --reword)
 	;;
 d | drop | -d | --drop)
 	export GIT_SEQUENCE_EDITOR="sed -i -e '1s/^/$pick /$drop /'"
+	;;
+a | auto | -a | --auto)
+	printf "%s^1" "$(auto_rebase)" | xargs -o git rebase -i
+	exit 0
 	;;
 *)
 	# No-op
