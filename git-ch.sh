397c2f5c724095ced9e381f5e47205df3d60501c
Sekkmer <sekkmer@gmail.com>
2023-06-05 16:43:38 +0200

    extend git ch with changes options

--- git-ch.sh
+++ git-ch.sh
@@ -9,23 +9,49 @@ branch_to_checkout=$(if [ "$branch_count" -eq 1 ]; then echo "$matching_branches
 
 changes=$(git status --porcelain)
 
+pop_stash=false
+checkout_args=()
+
 if [ -n "$changes" ]; then
-	echo "You have uncommitted changes. Do you want to stash them? [y/N]"
+	echo "You have uncommitted changes. Select an option:"
+	echo "[s] Stash them"
+	echo "[c] Continue with the changes"
+	echo "[f] Force checkout, discarding the changes"
+	echo "[p] Automatically stash and pop the changes"
+	echo "othewise Abort"
 	read -n 1 -r
 	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
+	case "$REPLY" in
+	[Ss])
 		git add -A
 		git stash
-	else
+		;;
+	[Cc])
+		# No-op
+		;;
+	[Ff])
+		checkout_args+=(--force)
+		;;
+	[Pp])
+		git add -A
+		git stash
+		pop_stash=true
+		;;
+	*)
 		echo "Aborting"
 		exit 1
-	fi
+		;;
+	esac
 fi
 
-git checkout "$branch_to_checkout"
+git checkout "$branch_to_checkout" "${checkout_args[@]-}"
 
 if echo "$branch_to_checkout" | grep -q "^feature/"; then
 	git pull
 else
 	git pull --rebase
+fi
+
+if $pop_stash; then
+	git stash pop
 fi
