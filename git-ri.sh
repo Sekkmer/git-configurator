79bc7d8387eca85132388f28e7ce8be670ffe0f4
Sekkmer <sekkmer@gmail.com>
2023-07-06 17:23:34 +0200

    add split option to ri

--- git-ri.sh
+++ git-ri.sh
@@ -29,6 +29,8 @@ function auto_rebase() {
 	echo "$earliest_target_hash"
 }
 
+split=false
+
 case "${1-}" in
 -e | --edit)
 	export GIT_SEQUENCE_EDITOR="sed -i -e '1s/^\(p\|pick\) /edit /'"
@@ -42,6 +44,9 @@ case "${1-}" in
 	export GIT_SEQUENCE_EDITOR="sed -i -e '1s/^\(p\|pick\) /drop /'"
 	shift
 	;;
+-s | --split)
+	split=true
+	;&
 -b | --break)
 	export GIT_SEQUENCE_EDITOR="sed -i '1i\\break'"
 	shift
@@ -58,3 +63,8 @@ esac
 
 commit=$(git select-commit --grep="^fixup\!\|^squash\!" --invert-grep) || exit 1
 printf "%s^1" "$commit" | xargs -o git rebase -i "$@"
+
+if $split; then
+	git cherry-pick --no-commit "$commit"
+	git reset
+fi
