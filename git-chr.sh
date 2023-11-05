eb671afd24608e7f36317a46a29e88171a789c39
Sekkmer <sekkmer@gmail.com>
2023-11-05 22:18:27 +0100

    fix: sk output search query

--- git-chr.sh
+++ git-chr.sh
@@ -4,9 +4,12 @@ set -euo pipefail
 
 git fetch --prune --no-tags --quiet
 
-remote_branches="$(git for-each-ref --format='%(refname:short)' refs/remotes/origin | grep -vE 'origin$')"
 local_branches="$(git for-each-ref --format='%(refname:short)' refs/heads)"
-only_remote_banches="$(echo "$remote_branches" | grep -vE "$local_branches")"
-branch="$(sk --ansi --no-sort --reverse --print-query --expect=ctrl-d <<<"$only_remote_banches" | tr -d '[:space:]')"
 
-git checkout "$branch" "${@}"
+git for-each-ref --format='%(refname:short)' refs/remotes/origin \
+	| grep -vE 'origin$' \
+	| grep -vE "$local_branches" \
+	| sk --ansi --no-sort --reverse --print-query --expect=ctrl-d \
+	| tail -1 \
+	| tr -d '[:space:]' \
+	| xargs -I {} git checkout "{}" "${@}"
