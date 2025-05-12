5f331436d9c65ecdfa53b8a77393117f9a1571b6
Sekkmer <sekkmer@gmail.com>
2025-05-12 22:08:59 +0200

    Accept upstream parameter for git pu

--- git-pu.sh
+++ git-pu.sh
@@ -2,7 +2,20 @@
 
 set -euo pipefail
 
-UPSTREAM=${1:-origin}
-BRANCH_NAME=$(git symbolic-ref --short HEAD)
+args=()
+upstream='origin'
 
-git push -u "$UPSTREAM" "$BRANCH_NAME" "$@"
+while [[ $# -gt 0 ]]; do
+	case $1 in
+	-u | --upstream)
+		upstream="$2"
+		shift 2
+		;;
+	*)
+		args+=("$1")
+		shift
+		;;
+	esac
+done
+
+git push --set-upstream "$upstream" "$(git symbolic-ref --short HEAD)" "${args[@]}"
